f2451840128b5527f1c75823f1b079f1b2b247be
zhangran653 <zhangran42@126.com>
2024-03-20 22:28:53 +0800

    setup tests

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,8 +1,13 @@
 import org.junit.Test;
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtField;
 
 public class Test1 {
     @Test
     public void test1() {
-        System.out.println("test1");
+        CtClass<?> l = Launcher.parseClass("class A { @Service()void m() { System.out.println(\"yeah\");} }");
+        System.out.println(l.prettyprint());
+        l.getMethods().forEach(System.out::print);
     }
 }
